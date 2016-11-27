46821c157b715d12a8c51451dd7c6cbdb18b8c98
Daniel Kornas <danielkornas@gmail.com>
2016-11-27 11:52:55 +0100

    Stateless functional component, default props

--- 3. Third component/js/app.jsx
+++ 3. Third component/js/app.jsx
@@ -7,29 +7,36 @@ var ThirdComponent = React.createClass({
         }
     },
 
-    addFive: function() {
+    addNumber: function(value){
         this.setState({
-            sum: this.state.sum + 5
+            sum: this.state.sum + value
         })
-
     },
 
-    addEight: function() {
-        this.setState({
-            sum: this.state.sum + 8
-        })
+    getDefaultProps: function(){ // Dafault properties
+        return{
+            numberOne: 5,
+            numberTwo: 8,
+            text: 'Add'
+        }
     },
 
     render: function() {
         return (
             <div>
-                <h2 id="test" className="test">Sum: <span>{this.state.sum}</span></h2> {/* instead of class="" have to be className , id is the same*/}
-                <button onClick = {this.addFive}> Add 5 to sum </button>
-                <button onClick = {this.addEight}> Add 8 to sum </button>
+                <h2 id="test" className="test">Sum: <span>{this.state.sum}</span></h2> {/* instead of class="" has to be className , id is the same*/}
+                <Button text = {`${this.props.text} ${this.props.numberOne}`} value={this.props.numberOne} clickHandler = {this.addNumber.bind(this, this.props.numberOne)} className="classname here"/>
+                <Button text = {`${this.props.text} ${this.props.numberTwo}`} value={this.props.numberTwo} clickHandler = {this.addNumber.bind(this, this.props.numberTwo)} />
             </div>
         )
     }
+
 });
+const Button = function(number){   // StateLess functional component
+    return(
+        <button value = {number.value} onClick = {number.clickHandler} className={number.style}>{number.text}</button>
+    )
+};
 ReactDOM.render(
     <ThirdComponent/>,
     counterBox
